6e795a0aeaa2889ddb381354785f40c35edcc1f1
Joshua <joshuanazareth97@gmail.com>
2018-07-14 00:26:11 +0530

    Added code to measure execution time. Updated comments to include problem question.

--- 3.py
+++ 3.py
@@ -1,5 +1,13 @@
+'''
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+Execution time = 0.232 secs
+'''
+
 from isPrime import isPrime
+import time
 n = int(input("Enter number to find largest prime factor: "))
+start = time.time()
 i = 2
 largest = 0
 while(i*i<n):
@@ -12,3 +20,5 @@ while(i*i<n):
     i += 1
 
 print(largest)
+end = time.time()
+print("Executed in {} secs".format(end - start))
