b0ac10c7d9d9f0885eaac5dd483d020e5bfaa81b
Joshua <joshuanazareth97@gmail.com>
2018-07-14 17:49:49 +0530

    Implemented sieve() function in Prime module

--- Prime.py
+++ Prime.py
@@ -9,9 +9,25 @@ def isPrime(n):
             i += 1
     return True
 
+def sieve(n):
+    p_n = {}
+    for i in range(2,n):
+        p_n[i] = True
+    for j in range(2,n):
+        if p_n[j] == True:
+            for a in range(2*j,n,j):
+                p_n[a] = False
+    return p_n
+
+
 if __name__ == "__main__":
-    n = int(input("Enter number to check for primality: "))
-    if(isPrime(n)):
-        print("{} is a Prime Number.".format(n))
-    else:
-        print("{} is not a Prime Number.".format(n))
+#    n = int(input("Enter number to check for primality: "))
+#    if(isPrime(n)):
+#        print("{} is a Prime Number.".format(n))
+#    else:
+#        print("{} is not a Prime Number.".format(n))
+    test = sieve(2000000)
+    n = int(input("Enter n < 2000000 to check for primality:"))
+    print("{} is".format(n),end=" ")
+    if not test[n]: print("not", end = " ")
+    print("prime.")
